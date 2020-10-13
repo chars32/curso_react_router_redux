5cf9553bf7ee4631e4bd8ea09bc623abc515f7e0
cristianiniguez <felizpc1@gmail.com>
2020-10-13 18:25:54 -0400

    crear nuestro archivo de rutas

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -10,7 +10,7 @@ import '../assets/styles/App.scss';
 
 const API = 'http://localhost:3000/initialState/';
 
-const App = () => {
+const Home = () => {
   const initialState = useInitialState(API);
   return (
     <div className='App'>
@@ -48,4 +48,4 @@ const App = () => {
   );
 };
 
-export default App;
+export default Home;
