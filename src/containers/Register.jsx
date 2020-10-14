e242eb2bb840cee6675563e741201cd5f6d606b1
cristianiniguez <felizpc1@gmail.com>
2020-10-14 18:51:28 -0400

    register

--- src/containers/Register.jsx
+++ src/containers/Register.jsx
@@ -1,22 +1,54 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import '../assets/styles/components/Register.scss';
 
-const Register = () => (
-  <section className='register'>
-    <section className='register__container'>
-      <h2>Regístrate</h2>
-      <form className='register__container--form'>
-        <input className='input' type='text' placeholder='Nombre' />
-        <input className='input' type='text' placeholder='Correo' />
-        <input className='input' type='password' placeholder='Contraseña' />
-        <button className='button' type='button'>
-          Registrarme
-        </button>
-      </form>
-      <Link to='/login'>Iniciar sesión</Link>
+const Register = () => {
+  const [form, setValues] = useState({
+    email: '',
+    name: '',
+    password: '',
+  });
+  const handleInput = (event) => {
+    setValues({ ...form, [event.target.name]: event.target.value });
+  };
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(form);
+  };
+  return (
+    <section className='register'>
+      <section className='register__container'>
+        <h2>Regístrate</h2>
+        <form className='register__container--form' onSubmit={handleSubmit}>
+          <input
+            className='input'
+            type='text'
+            placeholder='Nombre'
+            name='name'
+            onChange={handleInput}
+          />
+          <input
+            className='input'
+            type='text'
+            placeholder='Correo'
+            name='email'
+            onChange={handleInput}
+          />
+          <input
+            className='input'
+            type='password'
+            placeholder='Contraseña'
+            name='password'
+            onChange={handleInput}
+          />
+          <button className='button' type='submit'>
+            Registrarme
+          </button>
+        </form>
+        <Link to='/login'>Iniciar sesión</Link>
+      </section>
     </section>
-  </section>
-);
+  );
+};
 
 export default Register;
