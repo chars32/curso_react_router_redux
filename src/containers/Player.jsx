e994f90517c96a5eb805a402c34fef31d4033401
cristianiniguez <felizpc1@gmail.com>
2020-10-15 15:35:22 -0400

    arreglando la funcionalidad del player

--- src/containers/Player.jsx
+++ src/containers/Player.jsx
@@ -1,13 +1,22 @@
 import React from 'react';
+import '../assets/styles/components/Player.scss';
 
-const Player = () => {
+const Player = (props) => {
+  const {
+    match: {
+      params: { id },
+    },
+  } = props;
+  console.log(id);
   return (
     <div className='Player'>
       <video controls autoPlay>
         <source src='' type='video/mp4' />
       </video>
-      <div className='Player-black'>
-        <button type='button'>Regresar</button>
+      <div className='Player-back'>
+        <button type='button' onClick={() => props.history.goBack()}>
+          Regresar
+        </button>
       </div>
     </div>
   );
