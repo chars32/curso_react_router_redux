cb7e5ecb8be73061ff005a4666b6e4f2963c2d99
cristianiniguez <felizpc1@gmail.com>
2020-10-15 18:24:39 -0400

    validaciones

--- src/containers/Register.jsx
+++ src/containers/Register.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { registerRequest } from '../actions';
+import Header from '../components/Header';
 import '../assets/styles/components/Register.scss';
 
 const Register = (props) => {
@@ -19,38 +20,41 @@ const Register = (props) => {
     props.history.push('/');
   };
   return (
-    <section className='register'>
-      <section className='register__container'>
-        <h2>Regístrate</h2>
-        <form className='register__container--form' onSubmit={handleSubmit}>
-          <input
-            className='input'
-            type='text'
-            placeholder='Nombre'
-            name='name'
-            onChange={handleInput}
-          />
-          <input
-            className='input'
-            type='text'
-            placeholder='Correo'
-            name='email'
-            onChange={handleInput}
-          />
-          <input
-            className='input'
-            type='password'
-            placeholder='Contraseña'
-            name='password'
-            onChange={handleInput}
-          />
-          <button className='button' type='submit'>
-            Registrarme
-          </button>
-        </form>
-        <Link to='/login'>Iniciar sesión</Link>
+    <>
+      <Header isRegister />
+      <section className='register'>
+        <section className='register__container'>
+          <h2>Regístrate</h2>
+          <form className='register__container--form' onSubmit={handleSubmit}>
+            <input
+              className='input'
+              type='text'
+              placeholder='Nombre'
+              name='name'
+              onChange={handleInput}
+            />
+            <input
+              className='input'
+              type='text'
+              placeholder='Correo'
+              name='email'
+              onChange={handleInput}
+            />
+            <input
+              className='input'
+              type='password'
+              placeholder='Contraseña'
+              name='password'
+              onChange={handleInput}
+            />
+            <button className='button' type='submit'>
+              Registrarme
+            </button>
+          </form>
+          <Link to='/login'>Iniciar sesión</Link>
+        </section>
       </section>
-    </section>
+    </>
   );
 };
 
